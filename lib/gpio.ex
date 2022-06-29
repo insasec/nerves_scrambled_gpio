774d9da9a4a348cd976442e9811e76cccaa6200a
Udo Schneider <Udo.Schneider@insasec.com>
2022-06-29 19:51:23 +0200

    Initial commit

---
+++ lib/gpio.ex
@@ -0,0 +1,18 @@
+defmodule Gpio do
+  @moduledoc """
+  Documentation for Gpio.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gpio.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

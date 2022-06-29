a32fb99b42cbd343fddefe895a709cb0c34846ac
Udo Schneider <Udo.Schneider@insasec.com>
2022-06-29 20:15:28 +0200

    Scrambled GPIO

--- lib/gpio.ex
+++ lib/gpio.ex
@@ -3,16 +3,20 @@ defmodule Gpio do
   Documentation for Gpio.
   """
 
-  @doc """
-  Hello world.
+  def test_gpio() do
+    gpio_number = 49
+    expected_label = "P9_23"
+    {:ok, actual_label} = File.read("/sys/class/gpio/gpio#{gpio_number}/label")
+    actual_label = String.trim(actual_label)
 
-  ## Examples
-
-      iex> Gpio.hello
-      :world
-
-  """
-  def hello do
-    :world
+    if expected_label == actual_label do
+      IO.puts(
+        "PASS - Actual label (#{actual_label}) matches expected label (#{expected_label}) for GPIO #{gpio_number}"
+      )
+    else
+      IO.puts(
+        "FAIL - Actual label (#{actual_label}) does not match expected label (#{expected_label}) for GPIO #{gpio_number}"
+      )
+    end
   end
 end
